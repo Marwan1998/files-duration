c70ea56c0ed009d7770271e18c8e944a012ec2ce
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-08-15 23:19:34 +0200

    all done, ready to use

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const fs = require("fs");
-const {getVideoDurationInSeconds} = require("get-video-duration");
+const { getVideoDurationInSeconds } = require("get-video-duration");
+const { exit } = require("process");
 
 // const pathName = "C:/Users/Vendetta/Desktop/renameFolder/";
 const pathName = "renameFolder/";
 
-let durSum = 0;
-
-fs.readdir(pathName, (err, files) => {
-  if (!err) {
-    files.forEach(async (file) => {
-      const fullPath = pathName + file;
-      getVideoDurationInSeconds(fullPath).then((duration) => {
-        durSum += duration;
-        console.log("durSum inside the methode: " + durSum);
-      });
-    });
-  } else {
-    console.error("Could not do it", err);
-    process.exit(1);
-  }
+const readDirPromise = new Promise((resolve, reject) => {
+  // console.time('start');
+  let durSum = 0;
+  fs.readdir(pathName, async (err, files) => {
+    if (!err) {
+      for (const file of files) {
+        const fullPath = pathName + file;
+        await getVideoDurationInSeconds(fullPath).then((duration) => {
+          durSum += duration;
+          // console.log("durSum inside the methode: " + durSum);
+        });
+      }
+      resolve(durSum);
+      // console.timeEnd('start');
+    } else {
+      console.error("Could not do it", err);
+      reject(err);
+    }
+  });
 });
 
+readDirPromise.then(durSum => {
+  const time = {
+    Seconds: getMinutes(durSum).sec + 's',
+    minutes: getMinutes(durSum).min + 'm',
+    hours: getMinutes(durSum).hour + 'h',
+  }
+  console.table(time);
+}).catch(err => {
+  console.log('something went wrong ', err);
+  exit(1);
+});
 
-console.log("duration Sum is: " + durSum);
+function getMinutes(num) {
+  return {
+    sec: Math.round(num),
+    min: Math.round((num / 60) * 100) / 100,
+    hour: Math.round((num / (60 * 60)) * 100) / 100,
+  };
+}
